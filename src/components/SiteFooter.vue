ae1fd7ae09b4a2b02b66eddf1aa4d49dbcf11611
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-03-18 16:13:20 +0000

    Changing color green on gridsome

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,7 +2,7 @@
     <footer class="text-gray-700 text-sm leading-normal flex flex-wrap justify-between mx-auto max-w-3xl px-6 sm:px-12">
         <div class="w-full sm:w-1/2 mb-4 sm:mb-0">
             <p><a :href="$static.metadata.siteUrl" class="text-orange-700 border-b border-transparent hover:border-green-700 transition-border-color">{{ $static.metadata.siteName }}</a> by <a href="https://threenine.co.uk" class="text-orange-700  border-b border-transparent hover:border-orange-400 transition-border-color">threenine.co.uk</a></p>
-            <p>Built with <a href="https://gridsome.org" class="border-b border-transparent hover:border-gray-400 transition-border-color">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Tailwind CSS</a>.</p>
+            <p>Built with <a href="https://gridsome.org" class="text-green-700 border-b border-transparent hover:border-green-400 transition-border-color">Gridsome</a> &amp; <a href="https://tailwindcss.com/" class="border-b border-transparent hover:border-gray-400 transition-border-color">Tailwind CSS</a>.</p>
         </div>
     </footer>
 </template>
