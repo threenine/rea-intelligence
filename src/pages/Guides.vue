bd0bb24791318e14443ca8ae8aed59c450bbb534
Gary Woodfine <gary.woodfine@threenine.co.uk>
2020-03-18 22:58:32 +0000

    configure guides

---
+++ src/pages/Guides.vue
@@ -0,0 +1,15 @@
+<template>
+    <Layout>
+        <h1>Guides</h1>
+    </Layout>
+</template>
+
+<script>
+    export default {
+        name: "Guides"
+    }
+</script>
+
+<style scoped>
+
+</style>
